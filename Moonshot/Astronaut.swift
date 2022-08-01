280e90beaf7425b612320696327e1295422f5b15
CNLPY <calpay@yahoo.com>
2022-08-01 14:08:13 +0300

    some formatting done

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Can on 28.07.2022.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
